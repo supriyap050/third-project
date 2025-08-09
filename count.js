85914b1197712d4e42f7c5291e2efbb0b0742e95
supriya <supriyap0522@gmail.com>
2025-08-09 18:34:12 +0530

    Add number increment/decrement program

---
+++ count.js
@@ -0,0 +1,32 @@
+
+let count = 0;
+
+      function showCount() {
+        document.getElementById("count").innerText = count;
+      }
+
+       function increment () {
+        count++;
+         showCount();
+    }
+
+      function decrement() {
+        count--;
+        showCount();
+      }
+
+      function add() {
+        let value = Number(document.getElementById("input").value);
+        if (value) {
+          count = count + value;
+          showCount();
+        }
+      }
+
+      function subtract() {
+        let value = Number(document.getElementById("input").value);
+        if (value) {
+          count = count - value;
+          showCount();
+        }
+      }
